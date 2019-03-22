dfd1936fc827d92cbaf50aa8a20d7382cd966f28
nirnuhsimel@gmail.com <nirnuhsimel@gmail.com>
2019-03-22 16:48:27 +0300

    Add content in index.

--- ip-week-two/js/scripts.js
+++ ip-week-two/js/scripts.js
@@ -1,17 +1,25 @@
-var base = parseFloat (prompt("enter the base"));
-var height = parseFloat (prompt("enter the height"));
-var hypotenuse = parseFloat (prompt("enter the hypotenuse"));
- 
-if( height===base && height === hypotenuse) {
-    alert ("it's equlateral");
+function checkinputs()
+{
+/* Var = parseInt(document.Name_Of_Element_Form.Field_Name(Input).value); */
+/* Input Fields */
+base = parseInt(document.Triangle_Inputs.input1.value);
+height = parseInt(document.Triangle_Inputs.input2.value);
+hypotenuse = parseInt(document.Triangle_Inputs.input3.value);
+
+/*var sides = function(base,height,hypotenuse){
+  return(base+height>hypotenuse && height + hypotenuse>base && hypotenuse + base>height)
+}*/
+if(height === base && height === hypotenuse) {
+  alert ("it's equlateral");
 } else if ( base === height || hypotenuse === base || hypotenuse === height) {
-    alert ( "it's isoceles");
+  alert ( "it's isoceles");
 } else if ( base !== height && height !== base && hypotenuse !== height) {
-    if (height + base > hypotenuse || base + hypotenuse > height || height + hypotenuse > base) {
-        if ( height + base <= hypotenuse || hypotenuse + base <= height || hypotenuse + height <= base) {
-            alert ("cannot form a triangle");
-        } else {
-            alert("it's a scalene");
-        }
-    }
+  if (height + base > hypotenuse || base + hypotenuse > height || height + hypotenuse > base) {
+      if ( height + base <= hypotenuse || hypotenuse + base <= height || hypotenuse + height <= base) {
+          alert ("cannot form a triangle");
+      } else {
+          alert("it's a scalene");
+      }
+  }
+}
 }
